d12c48606ac466597232bada4db365a5f3174f16
Filip <filipnaklicki@gmail.com>
2021-03-28 22:28:47 +0200

    Add project files.

---
+++ GreenGuys/Data/Mocks/MockCategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using GreenGuys.Data.Intefaces;
+using GreenGuys.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreenGuys.Data.Mocks
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> Categories
+        {
+            get
+            {
+                return new List<Category>
+                {
+                    new Category{ CategoryName = "Common House Plants", Description = "Common and popular plants found inside many homes"},
+                    new Category{ CategoryName = "Flowering Plants", Description = "It's impossible to resist growing flowering plant types indoors."},
+                    new Category{ CategoryName = "Cactus Plants", Description = "If you have enough light and warmth you can grow cacti."},
+                    new Category{ CategoryName = "Easy & Low Light", Description = "Plants best for the begginers"},
+                    new Category{ CategoryName = "Unusual Plants", Description = "A collection of plants grown indoors that have an unusual appearance"}
+                };
+            }
+        }
+    }
+}
---
+++ GreenGuys/Data/Models/Plant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreenGuys.Data.Models
+{
+    public class Plant
+    {
+        public int PlantId { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public string ImageURL { get; set; }
+        public string ImageThumbnailURL { get; set; }
+        public decimal Price { get; set; }
+        public bool IsMostPopular { get; set; }
+        public int CategoryId { get; set; }
+        public bool InStock { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
