16583115831ba81a2766ac54832d7aa0adaa5dac
Filip <filipnaklicki@gmail.com>
2021-03-30 19:31:08 +0200

    Added DbContext

--- GreenGuys/Startup.cs
+++ GreenGuys/Startup.cs
@@ -1,10 +1,12 @@
+using GreenGuys.Data;
 using GreenGuys.Data.Intefaces;
 using GreenGuys.Data.Mocks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,10 +16,22 @@ namespace GreenGuys
 {
     public class Startup
     {
+        private IConfigurationRoot _configurationRoot;
+
+        public Startup(Microsoft.Extensions.Hosting.IHostEnvironment hostingEnvironment)
+        {
+            _configurationRoot = new ConfigurationBuilder()
+                .SetBasePath(hostingEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LingkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(_configurationRoot.GetConnectionString("DefaultConnection")));
             services.AddTransient<IPlantRepository, MockPlantRepository>();
             services.AddTransient<ICategoryRepository, MockCategoryRepository>();
             services.AddMvc(options => options.EnableEndpointRouting=false);
